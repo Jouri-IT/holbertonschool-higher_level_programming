0652f40625b95768b12210a9208f299801b93069
Jouri-IT <jouri.f.alsulaiman@gmail.com>
2026-06-06 11:02:30 +0300

    Solve task 5 explicit variable import assignment

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Import variable a from variable_load_5.py and print its value"""
-
 if __name__ == "__main__":
     from variable_load_5 import a
-    print(a)
+
+    print("{}".format(a))
