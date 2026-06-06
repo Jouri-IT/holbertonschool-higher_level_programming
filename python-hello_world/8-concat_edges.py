766f67e3f544a32d8817e7fc1e6776b8c74687f9
Jouri-IT <jouri.f.alsulaiman@gmail.com>
2026-06-06 10:40:15 +0300

    Solve task 6 with exact slicing and permissions

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python"
-print("object-oriented" + " programming" + " with Python")
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[38:67] + str[106:111] + str[0:6]
+print(str)
